95aef6d6086040fc910d6e96d583f74305732ce0
Stefanie <stefcodes@gmail.com>
2023-04-19 22:05:19 -0700

    change vibe

--- components/DropDown.tsx
+++ components/DropDown.tsx
@@ -10,14 +10,14 @@ function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
 
-export type VibeType = "Professional" | "Casual" | "Funny";
+export type VibeType = "Fancy" | "Quick" | "Sweet";
 
 interface DropDownProps {
   vibe: VibeType;
   setVibe: (vibe: VibeType) => void;
 }
 
-let vibes: VibeType[] = ["Professional", "Casual", "Funny"];
+let vibes: VibeType[] = ["Fancy", "Quick", "Sweet"];
 
 export default function DropDown({ vibe, setVibe }: DropDownProps) {
   return (
