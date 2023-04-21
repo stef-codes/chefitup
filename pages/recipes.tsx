95d442c0b32d44bb7ffb1ea0d968a9e546de5d1d
Stefanie <stefcodes@gmail.com>
2023-04-21 13:39:00 -0700

    create page with a grid

---
+++ pages/recipes.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from "next";
+
+const NewPage: NextPage = () => {
+  return (
+    <div className="grid grid-rows-4 grid-flow-col gap-4">
+      <div>01</div>
+      <div>02</div>
+      <div>03</div>
+      <div>04</div>
+      <div>05</div>
+      <div>06</div>
+      <div>07</div>
+      <div>08</div>
+      <div>09</div>
+    </div>
+  );
+};
+
+export default NewPage;
