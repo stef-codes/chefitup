35d783eabcca9a7b30fcf24b1dbcf43b06062379
Stefanie <stefcodes@gmail.com>
2023-04-25 21:57:25 -0700

    fix: change class to className

--- pages/index.tsx
+++ pages/index.tsx
@@ -179,7 +179,7 @@ const Home: NextPage = () => {
                       toast("Recipe downloaded", { icon: "👨‍🍳" });
                     }}
                   >
-                    <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+                    <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
                     Download
                   </button>
                 </div>
