9149a3d2da5ee0fa46b16fef4534deba7f9fad16
Stefanie <stefcodes@gmail.com>
2023-04-25 20:46:22 -0700

    add download button and icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -160,6 +160,7 @@ const Home: NextPage = () => {
                 <div className="btnDiv">
                   <button
                     id="downloadBtn"
+                    className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
                     onClick={() => {
                       const doc = new jsPDF();
                       doc.text(generatedRecipes.toString(), 10, 10);
@@ -167,6 +168,7 @@ const Home: NextPage = () => {
                       toast("Recipe downloaded", { icon: "👨‍🍳" });
                     }}
                   >
+                    <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
                     Download
                   </button>
                 </div>
