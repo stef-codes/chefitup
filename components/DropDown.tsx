f6c1de07ebadd7ff55fb5eb67f6856937a673e5e
Stefanie <stefcodes@gmail.com>
2023-05-18 10:08:44 -0700

    add diet goals

--- components/DropDown.tsx
+++ components/DropDown.tsx
@@ -10,14 +10,14 @@ function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
 
-export type VibeType = "Indian" | "Spanish" | "Southern";
+export type VibeType = "Maintain Weight" | "Weight Gain" | "Weight Loss";
 
 interface DropDownProps {
   vibe: VibeType;
   setVibe: (vibe: VibeType) => void;
 }
 
-let vibes: VibeType[] = ["Indian", "Spanish", "Southern"];
+let vibes: VibeType[] = ["Maintain Weight", "Weight Gain", "Weight Loss"];
 
 export default function DropDown({ vibe, setVibe }: DropDownProps) {
   return (
