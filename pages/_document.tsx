27a05b13cfb28b002e25a3bb2bb2e3fe4061af02
Stefanie <stefcodes@gmail.com>
2023-06-07 15:22:30 -0700

    remove meta info

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,28 +8,29 @@ class MyDocument extends Document {
           {/* <link rel="icon" href="/favicon.ico" /> */}
           <meta
             name="description"
-            content="Generate your next Twitter bio in seconds."
+            content="Generate your next Love Note in seconds."
           />
           <meta property="og:site_name" content="twitterbio.com" />
           <meta
             property="og:description"
-            content="Generate your next Twitter bio in seconds."
+            content="Generate your next Love Note in seconds."
           />
-          <meta property="og:title" content="Twitter Bio Generator" />
+          <meta property="og:title" content="Love Note Generator" />
           <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Twitter Bio Generator" />
+          <meta name="twitter:title" content="Love Note Generator" />
           <meta
             name="twitter:description"
-            content="Generate your next Twitter bio in seconds."
+            content="Generate your next Love Note in seconds."
           />
-          <meta
+          {/* get images for meta tags */}
+          {/* <meta
             property="og:image"
             content="https://twitterbio.com/og-image.png"
           />
           <meta
             name="twitter:image"
             content="https://twitterbio.com/og-image.png"
-          />
+          /> */}
         </Head>
         <body>
           <Main />
