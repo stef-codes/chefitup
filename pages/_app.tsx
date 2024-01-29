eb1ca3dc0f1d7ffc49ca12f88fd13ea6da684bc7
Stefanie <stefcodes@gmail.com>
2024-01-29 12:26:55 -0700

    update next.js version and add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
       <Analytics />
+      <GoogleAnalytics trackPageViews />
     </>
   );
 }
